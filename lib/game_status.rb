8282fd6a976a3e85be5d6baaea08c2d0aac68f72
Matt Hough <matt@hough.tv>
2017-09-07 22:40:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,7 +34,7 @@ def won?(board) #accept board argument and return true for win & false if no win
     end
 
     if trigger == true
-      return win_
+      return win_combination
 
     position_1 = board[win_index_1] #stores the value of the index matching the winning combination in a variable to check if it is a winner.
     position_2 = board[win_index_2]
