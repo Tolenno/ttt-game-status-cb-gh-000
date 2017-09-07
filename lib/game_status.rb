45c95c8820c497dedf4eb747b6094c6bf8277efe
Matt Hough <matt@hough.tv>
2017-09-07 21:54:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,7 @@ def won?(board) #accept board argument and return true for win & false if no win
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      
+      return win_combination
     end
   end
 end
