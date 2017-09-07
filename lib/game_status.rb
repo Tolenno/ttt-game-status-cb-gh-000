4316a751a9ffe2b83e113d29f7d958f31a5a0876
Matt Hough <matt@hough.tv>
2017-09-07 22:45:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,6 +34,7 @@ def won?(board) #accept board argument and return true for win & false if no win
       puts checker
     end
 
+puts triger
     if trigger == true
       return win_combination
     else
