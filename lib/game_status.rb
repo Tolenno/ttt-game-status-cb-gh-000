b7f8d29eb9c631ad7af46dc0aea59911d2bd61d0
Matt Hough <matt@hough.tv>
2017-09-07 21:31:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,6 +26,7 @@ WIN_COMBINATIONS = [
 def won?(board) #accept board argument and return true for win & false if no win yet. if there is a win will return winning combo indexes as an array.
   WIN_COMBINATIONS.each do |win_combo|
     if board[win_combo] == "X" && board[win_combo] == "O"
+      puts "TESTER"
   end
 end
 
