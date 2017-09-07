39b61f7246df88499b28ebbfddda33f6b07c1673
Matt Hough <matt@hough.tv>
2017-09-07 22:13:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,5 @@
 # Helper Method
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
