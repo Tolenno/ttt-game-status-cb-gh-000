69d4719bdf69bee14e3ce636c3f1596e72802344
Matt Hough <matt@hough.tv>
2017-09-07 23:23:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,8 +26,9 @@ WIN_COMBINATIONS = [
 def won?(board) #accept board argument and return true for win & false if no win yet. if there is a win will return winning combo indexes as an array.
   WIN_COMBINATIONS.each do |win_combination|
     if position_taken?(board, win_combination[0]) == position_taken?(board, win_combination[1]) && position_taken?(board, win_combination [1]) == position_taken?(board, win_combination[2])
-
-      if position_taken?(board, win_c[0]) && board[win_c[0]] == board[win_c[1]] && board[win_c[1]] == board[win_c[2]]
+      return win_combination
+    else
+      return false
   end
 end
 
