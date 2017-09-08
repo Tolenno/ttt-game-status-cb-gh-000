c075255bfd9a8c2b537c5f5f974e6a1ac80261bb
Matt Hough <matt@hough.tv>
2017-09-08 00:25:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,7 +55,7 @@ def over?(board) #returns true if game has been won, is a draw, or is full
 end
 
 def winner(board)
-  if !won?(board) == false && won?_board[0] == "X"
+  if !won?(board) == false && won?(board)[0] == "X"
     return "X"
   elsif !won?(board) == false && won?(board)[0] == "O"
     return "O"
